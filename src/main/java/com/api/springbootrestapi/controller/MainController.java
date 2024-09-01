d37d771f38953fc75284775046a6dc840259a398
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-09-01 19:05:25 +0530

    First Commit

--- src/main/java/com/api/springbootrestapi/controller/MainController.java
+++ src/main/java/com/api/springbootrestapi/controller/MainController.java
@@ -1,12 +1,12 @@
 package com.api.springbootrestapi.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String loadSwagger() {
 		return "redirect:/swagger-ui/index.html";
 	}
