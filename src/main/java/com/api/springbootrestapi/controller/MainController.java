4249af3014ccb19c58fbbe2eaeb147a68f8efc93
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-09-03 05:10:12 +0530

    Third Commit

--- src/main/java/com/api/springbootrestapi/controller/MainController.java
+++ src/main/java/com/api/springbootrestapi/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-	@GetMapping("/")
+	@GetMapping("/doc")
 	public String loadSwagger() {
 		return "redirect:/swagger-ui/index.html";
 	}
