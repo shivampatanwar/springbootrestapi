89129e1d24fed64473ef5a3d3c8bad077568274a
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-09-03 04:13:23 +0530

    First Commit

--- src/main/java/com/api/springbootrestapi/dto/Employee.java
+++ src/main/java/com/api/springbootrestapi/dto/Employee.java
@@ -1,17 +1,19 @@
 package com.api.springbootrestapi.dto;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
 import lombok.Data;
 
-@Entity
+//@Entity
+@Document
 @Data
 public class Employee {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+	
+	private String _id;
 	private String name;
 	private long mobile;
 	private double salary;
--- src/main/java/com/api/springbootrestapi/repository/EmployeeRepository.java
+++ src/main/java/com/api/springbootrestapi/repository/EmployeeRepository.java
@@ -3,9 +3,11 @@ package com.api.springbootrestapi.repository;
 import java.util.List;
 
 import com.api.springbootrestapi.dto.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+//public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+public interface EmployeeRepository extends MongoRepository<Employee, Integer> {
 
 	boolean existsByMobile(long mobile);
 
