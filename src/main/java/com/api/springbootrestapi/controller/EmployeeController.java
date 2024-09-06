92497fcc541a99387eb013e366fd676f660f6643
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-09-06 19:49:57 +0530

    Fourth Commit

--- src/main/java/com/api/springbootrestapi/controller/EmployeeController.java
+++ src/main/java/com/api/springbootrestapi/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import com.api.springbootrestapi.helper.ResponseStructure;
 import com.api.springbootrestapi.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -18,9 +19,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/v1")
 public class EmployeeController {
 	@Autowired
