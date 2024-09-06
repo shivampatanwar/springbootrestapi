38afd93d2471f03e7be200041b4ba886137ceead
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-09-06 21:54:54 +0530

    Sixth Commit

--- src/main/java/com/api/springbootrestapi/controller/EmployeeController.java
+++ src/main/java/com/api/springbootrestapi/controller/EmployeeController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
-import jakarta.ws.rs.Produces;
 
 
 @RestController
